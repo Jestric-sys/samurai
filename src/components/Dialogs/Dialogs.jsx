68db77b01dced4277c21285ece07dc9795fbd116
Jestric-sys <poltos194@gmail.com>
2022-08-08 12:45:07 +0300

    lesson 22

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/1" className={({ isActive }) => isActive ? s.active : undefined}>Denis</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    Vladimir
+                </div>
+                <div className={s.dialog}>
+                    Tommy
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Hou ara you?</div>
+                <div className={s.message}>Welcome new chat</div>
+            </div>
         </div>
     )
 };
