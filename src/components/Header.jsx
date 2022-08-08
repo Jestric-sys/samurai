021afa7b12b304b6627001249e201c031ac7c92d
Jestric-sys <poltos194@gmail.com>
2022-08-08 10:12:33 +0300

    lesson 14

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img src='https://www.meme-arsenal.com/memes/5ccc9b0b8134a0d77109dda7baf6cc10.jpg'></img>
         </header>
     )
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className="content">
-            <div>
+        <div className={s.content}>
+            <div className={s.content_header}>
                 <img src="https://steamuserimages-a.akamaihd.net/ugc/1691652784003611560/9368476CF11779DC22928537B592068DD3A0F836/" alt="img content" />
             </div>
             <div>
@@ -15,9 +16,9 @@ const Profile = () => {
                     New Post
                 </div>
                 <div>
-                    <div>post 1</div>
-                    <div>post 2</div>
-                    <div>post 3</div>
+                    <div className={s.item}>post 1</div>
+                    <div className={s.item}>post 2</div>
+                    <div className={s.item}>post 3</div>
                 </div>
             </div>
         </div>
