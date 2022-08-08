bcf4f265e24c5c44668692cbe79e43e814e481cc
Jestric-sys <poltos194@gmail.com>
2022-08-08 16:12:23 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
-import News from './components/News/News';
-import Music from './components/Music/Music';
-import Settings from './components/Settings/Settings';
+import Profile from './components/content/Profile/Profile';
+import Dialogs from './components/content/Dialogs/Dialogs';
+import News from './components/content/News/News';
+import Music from './components/content/Music/Music';
+import Settings from './components/content/Settings/Settings';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 const App = (props) => {
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Header.module.css';
 
 const Header = () => {
     return (
         <header className={s.header}>
-            <img src='https://www.meme-arsenal.com/memes/5ccc9b0b8134a0d77109dda7baf6cc10.jpg'></img>
+            <NavLink to='/'>
+                <img src='https://www.meme-arsenal.com/memes/5ccc9b0b8134a0d77109dda7baf6cc10.jpg'></img>
+            </NavLink>
         </header>
     )
 };
