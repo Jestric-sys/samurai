ee96867bf234bfa78b7790de17d7b5b18de575ff
Jestric-sys <poltos194@gmail.com>
2022-08-24 09:05:09 +0300

    lesson 37

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,55 +1,57 @@
-let rerenderEntireTree = () => {
-    console.error('Not subscribe');
-};
-
-const state = {
-    profilePage: {
-        posts: [
-            {id: 1, img: 'https://icons-for-free.com/download-icon-avatar-1320568024619304547_512.png', message: 'Hi, how ara you?', like: 15},
-            {id: 2, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI3vvVZ-pOGsyhaNEm9s-tm96lh7OGxJrpPQ&usqp=CAU', message: 'My first post', like: 20}
-        ],
-        dialogs: [
-            {id: 1, img: 'https://icons-for-free.com/download-icon-avatar-1320568024619304547_512.png', name: 'Denis'},
-            {id: 2, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI3vvVZ-pOGsyhaNEm9s-tm96lh7OGxJrpPQ&usqp=CAU', name: 'Vladimir'},
-            {id: 3, img: 'https://icon-library.com/images/avatar-icon-free/avatar-icon-free-15.jpg', name: 'Tommy'}
-        ],
-        friends: [
-            {id: 1, img: 'https://icons-for-free.com/download-icon-avatar-1320568024619304547_512.png', name: 'Denis'},
-            {id: 2, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI3vvVZ-pOGsyhaNEm9s-tm96lh7OGxJrpPQ&usqp=CAU', name: 'Vladimir'},
-            {id: 3, img: 'https://icon-library.com/images/avatar-icon-free/avatar-icon-free-15.jpg', name: 'Tommy'}
-        ],
-        newPostText: 'berserk'
+const store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, img: 'https://icons-for-free.com/download-icon-avatar-1320568024619304547_512.png', message: 'Hi, how ara you?', like: 15},
+                {id: 2, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI3vvVZ-pOGsyhaNEm9s-tm96lh7OGxJrpPQ&usqp=CAU', message: 'My first post', like: 20}
+            ],
+            dialogs: [
+                {id: 1, img: 'https://icons-for-free.com/download-icon-avatar-1320568024619304547_512.png', name: 'Denis'},
+                {id: 2, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI3vvVZ-pOGsyhaNEm9s-tm96lh7OGxJrpPQ&usqp=CAU', name: 'Vladimir'},
+                {id: 3, img: 'https://icon-library.com/images/avatar-icon-free/avatar-icon-free-15.jpg', name: 'Tommy'}
+            ],
+            friends: [
+                {id: 1, img: 'https://icons-for-free.com/download-icon-avatar-1320568024619304547_512.png', name: 'Denis'},
+                {id: 2, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI3vvVZ-pOGsyhaNEm9s-tm96lh7OGxJrpPQ&usqp=CAU', name: 'Vladimir'},
+                {id: 3, img: 'https://icon-library.com/images/avatar-icon-free/avatar-icon-free-15.jpg', name: 'Tommy'}
+            ],
+            newPostText: 'berserk'
+        },
+        messagePage: {
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'Hou ara you?'},
+                {id: 3, message: 'Welcome new chat'}
+            ]
+        }
     },
-    messagePage: {
-        messages: [
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'Hou ara you?'},
-            {id: 3, message: 'Welcome new chat'}
-        ]
+    getState() {
+        return this._state;
+    },
+    _callSubscriber() {
+        console.error('Not subscribe');
+    },
+    addPost() {
+        let newPost = {
+            id: +(new Date()),
+            img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPFJ__HxmXSqCktfWMBg3XNM8n9vVkCr5tsQ&usqp=CAU',
+            message: this._state.profilePage.newPostText,
+            like: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        console.log(this._state);
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer; // Наблюдатель
     }
 };
 
-window.state = state;
+window.store = store;
 
-export const addPost = () => {
-    let newPost = {
-        id: +(new Date()),
-        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPFJ__HxmXSqCktfWMBg3XNM8n9vVkCr5tsQ&usqp=CAU',
-        message: state.profilePage.newPostText,
-        like: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-};
-
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer; // Наблюдатель
-};
-
-export default state;
+export default store;
