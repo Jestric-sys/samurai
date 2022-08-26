5ff09932d78739e7e375d817f552622ca1091308
Jestric-sys <poltos194@gmail.com>
2022-08-26 17:00:08 +0300

    lesson 51

--- src/components/content/Users/Users.jsx
+++ src/components/content/Users/Users.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import s from './Users.module.css';
+import * as axios from 'axios';
 
 const Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-            {id: 1, photo: 'https://icons-for-free.com/download-icon-avatar-1320568024619304547_512.png', followed: false, fullName: 'Denis', status: 'I am boss', location: {city: 'Yaroslavl', country: 'Russia'}},
-            {id: 2, photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQI3vvVZ-pOGsyhaNEm9s-tm96lh7OGxJrpPQ&usqp=CAU', followed: false, fullName: 'Vladimir', status: 'Hello my friends', location: {city: 'Moscow', country: 'Russia'}},
-            {id: 3, photo: 'https://icon-library.com/images/avatar-icon-free/avatar-icon-free-15.jpg', followed: false, fullName: 'Tommy', status: 'Base', location: {city: 'Kostroma', country: 'Russia'}},
-            {id: 4, photo: 'https://icon-library.com/images/avatar-icon-free/avatar-icon-free-15.jpg', followed: true, fullName: 'Secret', status: 'Not base', location: {city: 'Ivanovo', country: 'Russia'}}
-        ]);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(res => props.setUsers(res.data.items));
     };
 
     return (
@@ -19,7 +16,7 @@ const Users = (props) => {
                     <div className={s.friends} key={u.id}>
                         <span className={s.card}>
                             <div>
-                                <img className={s.logo} src={u.photo} alt="image friends" />
+                                <img className={s.logo} src={u.photos.small ? u.photos.small : 'https://www.meme-arsenal.com/memes/39321a804031b9f9f89c520554104704.jpg'} alt="image friends" />
                             </div>
                             <div>
                                 { u.followed === true 
@@ -29,12 +26,12 @@ const Users = (props) => {
                         </span>
                         <span className={s.info}>
                             <span>
-                                <div>{u.fullName}</div>
-                                <div>{u.status}</div>
+                                <div>{u.name}</div>
+                                <div>{u.status ? u.status : 'status is not'}</div>
                             </span>
                             <span>
-                                <div>{u.location.country}</div>
-                                <div>{u.location.city}</div>
+                                <div>{u.location ? u.location.country : 'location is not'}</div>
+                                <div>{u.location ? u.location.city : 'city is not'}</div>
                             </span>
                         </span>
                     </div>
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -40,7 +40,7 @@ const usersReducer = (state = initialState, action) => {
             }
         };
         case ACTION.SET_USERS: {
-            return { ...state, users: [...action.users] };
+            return { ...state, users: action.users };
         };
         default: {
             return state
